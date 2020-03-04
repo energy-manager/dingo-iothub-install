ae8d956300ddd596abe6733f1f1223c6970c68c0
Alex Uleberg <alex@energymanager.no>
2020-03-04 09:41:05 +0100

    Some changes to bash file

--- install-dingo-iothub.sh
+++ install-dingo-iothub.sh
@@ -7,10 +7,8 @@ FILE_PATH='/usr/bin/dingo-iothub'
 APP_ID=""
 PROP_ID=""
 
-while getopts a:p: option
-  do
-    case "${option}"
-    in
+while getopts a:p: option do
+  case "${option}" in
     a) APP_ID="${OPTARG}";;
     p) PROP_ID="${OPTARG}";;
   esac
@@ -21,25 +19,25 @@ function _jq() {
 }
 
 function get_connection_string() {
-  SERVICES=$(curl -s 'http://localhost/bacnetws/apps/instances?alt=json')
+  SERVICES=$(curl -s "http://localhost/bacnetws/apps/instances?alt=json")
 
   for row in $(echo "${SERVICES}" | jq -r '.[] | @base64'); do
     APP_IDENTIFIER=$(_jq $row '.["app-identifier"]')
     STARTUP=$(_jq $row '.["start-at-startup"]')
 
-    if [ $APP_IDENTIFIER -eq $APP_ID ] && [ $STARTUP -eq 1 ]
+    if [ "$APP_IDENTIFIER" == "$APP_ID" ] && [ $STARTUP == "1" ]
     then
       OBJECT_ID=$(_jq $row '.["object-identifier"]')
       break
     fi
   done
 
-  PROPERTIES=$(curl -s 'http://localhost/bacnetws/apps/property-instances?filter=app-identifier%20eq%20'${OBJECT_ID}'&alt=json')
+  PROPERTIES=$(curl -s "http://localhost/bacnetws/apps/property-instances?filter=app-identifier%20eq%20'${OBJECT_ID}'&alt=json")
 
   for row in $(echo "${PROPERTIES}" | jq -r '.[] | @base64'); do
     PROP_IDENTIFIER=$(_jq $row '.["property-identifier"]')
 
-    if [[ $PROP_IDENTIFIER -eq $PROP_ID ]]
+    if [[ "$PROP_IDENTIFIER" == "$PROP_ID" ]]
     then
       CONNECTION_STRING=$(_jq $row '.["value"]')
       break
